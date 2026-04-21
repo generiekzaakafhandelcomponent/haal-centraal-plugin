178cd286001b71a51a14dafb8025fc8038a14dd1
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-04-21 12:30:21 +0200

    Fix app.module.ts: use HaalCentraalBrpPluginModule and haalCentraalBrpPluginSpecification

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -98,7 +98,7 @@ import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {environment} from "../environments/environment";
 import {registerDocumentenApiFormioUploadComponent, ZgwModule} from "@valtimo/zgw";
 
-import {SamplePluginModule, samplePluginSpecification,} from "@valtimo-plugins/haal-centraal";
+import {HaalCentraalBrpPluginModule, haalCentraalBrpPluginSpecification,} from "@valtimo-plugins/haal-centraal";
 
 export function tabsFactory() {
   return new Map<string, object>([
@@ -158,7 +158,7 @@ export function tabsFactory() {
     ProcessModule,
     ReactiveFormsModule,
     ResourceModule,
-    SamplePluginModule,
+    HaalCentraalBrpPluginModule,
     SecurityModule,
     SseModule,
     SwaggerModule,
@@ -190,7 +190,7 @@ export function tabsFactory() {
         objecttypenApiPluginSpecification,
         openNotificatiesPluginSpecification,
         openZaakPluginSpecification,
-        samplePluginSpecification,
+        haalCentraalBrpPluginSpecification,
         zakenApiPluginSpecification,
       ],
     },
